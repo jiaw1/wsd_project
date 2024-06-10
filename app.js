cd8b7e094c6d5739844f6079fde82f1f84d06cc3
jiaw1 <97261150+jiaw1@users.noreply.github.com>
2024-06-10 19:48:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-//deno run --allow-net --watch app.js
-//deno run --allow-net --watch app-run.js
-//deno run --allow-net --unstable --watch app-run.js
-// Remove-item alias:curl
-// crtl + p
-// Access token: ddp_DSLuqPMbOtNFYr6jAzvUxwFNkIsFfh3rcFVv
-
+import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
 import * as feedbacks from "./feedbacks.js";
+import * as courseController from "./courseController.js";
 
+const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 const app = new Hono();
 
-app.get("/feedbacks/:id", async (c) => {
-  const id = c.req.param("id");
-  const feedbackCount = await feedbacks.getFeedbackCount(id);
-  return c.text(`Feedback ${id}: ${feedbackCount}`);
+app.get("/courses/:courseId/feedbacks/:feedbackId", async (c) => {
+  const courseId = c.req.param("courseId");
+  const feedbackId = c.req.param("feedbackId");
+  const feedbackCount = await feedbacks.getFeedbackCount(courseId, feedbackId);
+  return c.text(`Feedback ${feedbackId}: ${feedbackCount}`);
 });
 
-app.post("/feedbacks/:id", async (c) => {
-  const id = c.req.param("id");
-  await feedbacks.incrementFeedbackCount(id);
-  return c.text(`OK`);
+app.post("/courses/:courseId/feedbacks/:feedbackId", async (c) => {
+  const courseId = c.req.param("courseId");
+  const feedbackId = c.req.param("feedbackId");
+  await feedbacks.incrementFeedbackCount(courseId, feedbackId);
+  return c.redirect(`/courses/${courseId}`);
 });
+
+app.get("/courses", courseController.showForm);
+app.get("/courses/:id", courseController.showCourse);
+app.post("/courses", courseController.createCourse);
+app.post("/courses/:id/delete", courseController.deleteCourse);
 
 export default app;
